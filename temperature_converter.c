a5bfb368b459e7e9e3eaaab77547135243fa76f5
Eduardo Poleo <eduardopoleo@gmail.com>
2019-08-25 15:53:08 -0400

    Add some comments

--- temperature_converter.c
+++ temperature_converter.c
@@ -13,7 +13,13 @@ int main() {
     far = start;
     cel = 5.0 / 9.0  * (far - 32);
 
-    printf("%3.0f %6.1f\n ", far, cel);
+    // 6.1 indicates that we want at least 6 characters long (which gives us padding)
+    // and that the precision of the float should be of 1 decimal
+    // 3.0 means that we only required 3 characters padding and without decimal
+
+    // %d is for integer
+    // %f is for float
+    printf("%3.0f %6.1d\n ", far, cel);
 
     start = start + step;
   }
